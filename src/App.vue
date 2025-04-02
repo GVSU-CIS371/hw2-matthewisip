141d2cca5ee77186b36cad27e78db9990933dcea
matthew.isip <isipm@mail.gvsu.edu>
2025-04-02 11:44:35 -0400

    HW2 Functionality

--- src/App.vue
+++ src/App.vue
@@ -17,10 +17,78 @@
         </template>
       </li>
     </ul>
-    <input type="text" placeholder="Beverage Name" />
-    <button>🍺 Make Beverage</button>
+
+    <ul>
+      <li>
+        <template v-for="base in beverageStore.bases" :key="base.id">
+          <label>
+            <input
+              type="radio"
+              name="baseBeverage"
+              :id="`r${base.id}`"
+              :value="base"
+              v-model="beverageStore.currentBase"
+            />
+            {{ base.name }}
+          </label>
+        </template>
+      </li>
+    </ul>
+    <!-- <BaseBeverage :color="currentBase.color" /> -->
+
+    <ul>
+      <li>
+        <template v-for="(cream, index) in beverageStore.creamers" :key="cream.id">
+          <label>
+            <input
+              type="radio"
+              name="creamer"
+              :id="`r${cream.id}`"
+              :value="cream"
+              :checked="index === 0"
+              v-model="beverageStore.currentCream"
+            />
+            {{ cream.name }}
+          </label>
+        </template>
+      </li>
+    </ul>
+
+    <ul>
+      <li>
+        <template v-for="(syrup, index) in beverageStore.syrups" :key="syrup.id">
+          <label>
+            <input
+              type="radio"
+              name="syrup"
+              :id="`r${syrup.id}`"
+              :value="syrup"
+              :checked="index===0"
+              v-model="beverageStore.currentSyrup"
+            />
+            {{ syrup.name }}
+          </label>
+        </template>
+      </li>
+    </ul>
+    <input type="text" placeholder="Beverage Name" v-model="beverageStore.currentName"/>
+    <button @click = "beverageStore.makeBeverage()">Make Beverage</button>
   </div>
-  <div id="beverage-container" style="margin-top: 20px"></div>
+  <div v-if="beverageStore.beverageList.length > 0">
+  <ul class="beverage-radio-list">
+    <li v-for="bev in beverageStore.beverageList" :key="bev.id">
+      <label>
+        <input
+          type="radio"
+          name="selectedBeverage"
+          :value="bev.id"
+          @change="beverageStore.showBeverage(bev)"
+        />
+        {{ bev.name }}
+      </label>
+    </li>
+  </ul>
+</div>
 </template>
 
 <script setup lang="ts">
@@ -41,6 +109,16 @@ html {
   background: linear-gradient(to bottom, #6e4228 0%, #956f5a 100%);
 }
 ul {
+  list-style: none;
+}
+.beverage-radio-list {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 1rem;
+  padding: 0;
+  margin: 0;
+}
+.beverage-radio-list li {
   list-style: none;
 }
 </style>
--- src/stores/beverageStore.ts
+++ src/stores/beverageStore.ts
@@ -1,15 +1,45 @@
 import { defineStore } from "pinia";
 import tempretures from "../data/tempretures.json";
+import bases from "../data/bases.json"
+import creamers from "../data/creamers.json"
+import syrups from "../data/syrups.json"
+import { BeverageType } from "../types/beverage";
 
 export const useBeverageStore = defineStore("BeverageStore", {
   state: () => ({
     temps: tempretures,
     currentTemp: tempretures[0],
+    bases: bases,
+    currentBase: bases[0],
+    creamers: creamers,
+    currentCream: creamers[0],
+    syrups: syrups,
+    currentSyrup: syrups[0],
+    currentName: "",
+    beverageList: [] as BeverageType[],
+    currentBeverage: null as BeverageType | null,
   }),
 
   actions: {
-    makeBeverage() {},
-    showBeverage() {},
+    makeBeverage() {
+      this.currentBeverage = {
+        name:this.currentName,
+        id: `${this.currentTemp}-${this.currentBase.id}-${this.currentCream.id}-${this.currentSyrup.id}`,
+        temp:this.currentTemp,
+        base:this.currentBase,
+        syrup:this.currentSyrup,
+        creamer:this.currentCream
+      }
+      this.beverageList.push(this.currentBeverage);
+    },
+    showBeverage(beverage: BeverageType) {
+      this.currentTemp = beverage.temp;
+      this.currentBase = beverage.base;
+      this.currentCream = beverage.creamer;
+      this.currentSyrup = beverage.syrup;
+      this.currentName = beverage.name;
+      this.currentBeverage = beverage;
+    },
   },
   persist: true,
 });
